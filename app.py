f6925061ec90a413bc59a228821ecddc0ede1f0d
jishaanujose <91037105+jishaanujose@users.noreply.github.com>
2021-11-04 14:10:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 # Flask utils
+.slugignore
 from werkzeug.utils import secure_filename
 import tensorflow_hub as hub
 from pydub import AudioSegment
